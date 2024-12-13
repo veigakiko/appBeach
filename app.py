2beb0682de7b4ccef5d1181916561d10bbc77342
Ricardo Oliveira <100167292+veigakiko@users.noreply.github.com>
2024-12-13 02:48:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ def run_insert(query, values):
         conn.commit()
         return True
     except Exception as e:
+        if conn:
+            conn.rollback()
         st.error(f"Error executing insert: {e}")
         return False
 
