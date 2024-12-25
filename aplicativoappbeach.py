fb8408293d1819b6d28f55a5afde4aa48acc460b
Ricardo Oliveira <100167292+veigakiko@users.noreply.github.com>
2024-12-25 06:49:52 -0300

    Update aplicativoappbeach.py

--- aplicativoappbeach.py
+++ aplicativoappbeach.py
@@ -760,19 +760,21 @@ def generate_invoice_for_printer(df):
 # Login Page
 #####################
 def login_page():
+    # Definição do formulário de login
     with st.form(key='login_form'):
         st.header("Beach Club")
         username = st.text_input("Nome de Usuário", key="login_username")
         password = st.text_input("Senha", type="password", key="login_password")
+        # Botão de submissão dentro do contexto do formulário
         submit_login = st.form_submit_button(label="Login", key="login_submit_button")
 
+    # Processamento após submissão
     if submit_login:
-        st.write("Botão de login pressionado.")
-        admin_username = os.getenv("ADMIN_USERNAME")
-        admin_password = os.getenv("ADMIN_PASSWORD")
-        caixa_username = os.getenv("CAIXA_USERNAME")
-        caixa_password = os.getenv("CAIXA_PASSWORD")
-        
+        admin_username = os.getenv("ADMIN_USERNAME", "admin")
+        admin_password = os.getenv("ADMIN_PASSWORD", "adminbeach")
+        caixa_username = os.getenv("CAIXA_USERNAME", "caixa")
+        caixa_password = os.getenv("CAIXA_PASSWORD", "caixabeach")
+
         if username == admin_username and password == admin_password:
             st.session_state.logged_in = True
             st.session_state.username = "admin"
@@ -785,6 +787,7 @@ def login_page():
             st.experimental_rerun()
         else:
             st.error("Nome de usuário ou senha incorretos.")
+
 
 #####################
 # Initialization
